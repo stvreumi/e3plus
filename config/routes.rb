f50bcc008f94aef54e0dd2ef13da7d651b8b0e0e
Jay Lin <jay101630@gmail.com>
2016-11-26 01:32:13 +0800

    add root controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :users
+  root to: 'root#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
