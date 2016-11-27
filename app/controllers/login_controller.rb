274464acff65ab702272bbf3939dc32e6ba8f88a
Jay Lin <jay101630@gmail.com>
2016-11-27 12:12:02 +0800

    add login action

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,20 @@
 class LoginController < ApplicationController
   def login
+      #login session
+      session[:user_id] = nil
+      if request.post?#login action
+          #authenticate user
+          user = User.authenticate(params[:account], params[:password])
+          #login successfully
+          if user
+              session[:user_id] = user_id#store user_id to session
+              redirect_to(:action => "index")#redirect_to index.html.erb
+          
+          #login fail => error msg
+          else
+              flash[:notice] = "Invalid user/password combination"
+          end
+      end
   end
 
   def logout
