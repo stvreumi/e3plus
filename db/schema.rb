85f07c0bf0d60062cbe2613b973dec0dcac2c66f
Jay Lin <jay101630@gmail.com>
2016-11-30 22:29:13 +0800

    add column identity to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128053009) do
+ActiveRecord::Schema.define(version: 20161130142244) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20161128053009) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "identity"
   end
 
 end
