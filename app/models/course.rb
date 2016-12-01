7875a5de86cdb23c0bfb1906136e1f04f211cdac
Jay Lin <jay101630@gmail.com>
2016-12-01 17:15:14 +0800

    create announce database

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,7 @@ class Course < ActiveRecord::Base
     #many-to-many associations between users & courses
     has_many :user_courseships
     has_many :users, :through => :user_courseships
+
+    #one-to-many associations between course & announces
+    has_many :announces
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130142244) do
+ActiveRecord::Schema.define(version: 20161201090715) do
+
+  create_table "announces", force: :cascade do |t|
+    t.string   "tag"
+    t.string   "topic"
+    t.text     "content"
+    t.datetime "deadline"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "course_id"
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
